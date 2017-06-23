8bc3901d2474360d615e55703665e25bc1c0b296
jonech <jone@live.hk>
2017-06-23 15:18:17 +1000

    click edit text half way

---
+++ src/app/reuseable/click_edit_long_text.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'click-edit-long-text',
+    template:
+    ``
+})
+
+export class ClickEditLongTextComponent implements OnInit
+{
+
+    constructor() { }
+
+    ngOnInit() { }
+}
