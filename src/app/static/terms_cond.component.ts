2d0297d4990643b288e881d125a8bdec8cf91d46
jonech <jone@live.hk>
2017-06-24 15:40:18 +1000

    terms and condition

---
+++ src/app/static/terms_cond.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'terms-cond-component',
+    templateUrl: 'terms_cond.component.html',
+    styleUrls: ['privacy_policy.component.css']
+})
+
+export class TermsConditionComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
