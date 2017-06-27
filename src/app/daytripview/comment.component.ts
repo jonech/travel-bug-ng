7b3d1e35e741d3f7a82cf35f51d84d108519177f
jonech <jone@live.hk>
2017-06-27 22:35:27 +1000

    decoration for user comment

--- src/app/daytripview/comment.component.ts
+++ src/app/daytripview/comment.component.ts
@@ -3,11 +3,31 @@ import { FirebaseObjectObservable, AngularFire } from 'angularfire2';
 
 @Component({
 	selector: 'comment-component',
+    styleUrls: ['comment.component.css'],
 	template:
 	`
-		<div>
-			{{ (_comment | async)?.date }}
-			{{ (_comment | async)?.comment }}
+		<div class="comment"
+            [ngClass]="{'even-row': isEven, 'odd-row': isOdd}">
+            <div class="section group">
+                <div class="col user-image">
+                    <going-user [goingUserId]="userId"></going-user>
+                </div>
+
+                <div class="col span_70">
+                    <div class="username">
+                        {{ (_user|async)?.firstName }}
+                    </div>
+
+                    <div class="datetime">
+                        {{ (_comment | async)?.date }}
+                    </div>
+
+                    <div class="message">
+                        {{ (_comment | async)?.comment }}
+                    </div>
+                </div>
+            </div>
+
 		</div>
 	`
 })
@@ -16,8 +36,13 @@ export class CommentComponent implements OnInit
 {
 	@Input() commentId : string;
 	@Input() userId : string;
+    @Input() index: number;
 
 	_comment: FirebaseObjectObservable<any>;
+    _user: FirebaseObjectObservable<any>;
+
+    isEven: boolean;
+    isOdd: boolean;
 
 	constructor(
 		private firebase: AngularFire
@@ -25,7 +50,15 @@ export class CommentComponent implements OnInit
 
 	ngOnInit()
 	{
-		console.log(this.commentId, this.userId);
 		this._comment = this.firebase.database.object(`/Comment/${this.commentId}`);
+        this._user = this.firebase.database.object(`/User/${this.userId}/UserDetails`);
+
+        if (this.index % 2 != 0) {
+            this.isOdd = true;
+            this.isEven = false;
+        }
+        else {
+            this.isEven = true;
+        }
 	}
 }
