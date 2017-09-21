cb241349d59841263484b86513f6b951c52fb143
Zhijin Li <qinqinlizhi1993@163.com>
2017-09-21 16:42:22 +1000

    import ngzorroantmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 import { Routing } from './app.routes';
 
