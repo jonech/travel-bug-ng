662d64094634d04a8ab562daa0e40a8c421187c6
Zhijin Li <ziyueli1993@outlook.com>
2017-12-05 14:17:59 +1100

    make register component responsive

--- src/app/auth/register.component.ts
+++ src/app/auth/register.component.ts
@@ -15,7 +15,7 @@ import{ AuthService } from '../_service/auth.service';
 @Component({
 	selector: 'register',
 	templateUrl: './register.component.html',
-	styleUrls: ['./register.component.css'],
+	styleUrls: ['./register.component.scss'],
 })
 
 export class RegisterComponent implements OnInit
