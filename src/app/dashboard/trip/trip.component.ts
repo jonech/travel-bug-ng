ecdd5e39ba9997cf46c40444d837c4b9d68f4e76
Jone <jone@live.hk>
2018-01-30 20:07:35 +1100

    fix edit delete trip click overlap

--- src/app/dashboard/trip/trip.component.ts
+++ src/app/dashboard/trip/trip.component.ts
@@ -2,23 +2,23 @@ import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/cor
 import { Router } from '@angular/router'
 import { NzModalService } from 'ng-zorro-antd';
 
-import { Trip } from '../../models';
-import { TripService } from '../../services/trip.service';
-import { EmitterService } from '../../services/event-emitter.service';
+import { Trip } from 'app/models';
+import { TripService } from 'app/services/trip.service';
+import { EmitterService } from 'app/services/event-emitter.service';
 @Component({
 	selector: 'trip',
   styleUrls: ['trip.component.scss'],
   template: `
-  <div routerLink="/daytrip/{{trip.id}}" nz-col [nzXs]="24" [nzSm]="24" [nzMd]="8" [nzLg]="8" [nzXl]="8" class="card-wrapper">
-    <nz-card class="card">
+  <div nz-col [nzXs]="24" [nzSm]="24" [nzMd]="8" [nzLg]="8" [nzXl]="8" class="card-wrapper">
+    <nz-card class="card" (click)="onClick($event, 'Trip')">
       <ng-template #body>
         <div class="card-top">
           <img alt="example" src="https://i.pinimg.com/736x/f7/ba/65/f7ba654d396fc5b904ccc2c2532435a5--eerie-photography-creative-photography.jpg"/>
         </div>
 
         <div class="tool-button">
-          <button nz-button (click)="editTrip()" [nzSize]="large"><i class="anticon anticon-edit"></i></button>
-          <button nz-button (click)="deleteTrip()" [nzSize]="large"><i class="anticon anticon-delete"></i></button>
+          <button nz-button (click)="onClick($event, 'Edit')" [nzSize]="large"><i class="anticon anticon-edit"></i></button>
+          <button nz-button (click)="onClick($event, 'Delete')" [nzSize]="large"><i class="anticon anticon-delete"></i></button>
         </div>
 
         <div class="date-box">
@@ -52,9 +52,27 @@ export class TripComponent implements OnInit {
     console.log(this.trip);
 	}
 
-	showTripDetail(id: string) {
-		if (id != null) {
-			this.router.navigate(['/trip', id]);
+  onClick(event: Event, button: string) {
+    event.stopPropagation();
+
+    switch (button) {
+      case 'Trip':
+        this.toTripActivity();
+        break;
+      case 'Delete':
+        this.deleteTrip();
+        break;
+      case 'Edit':
+        this.editTrip();
+        break;
+      default:
+        break;
+    }
+  }
+
+  toTripActivity() {
+    if (this.trip.id != null) {
+			this.router.navigate(['/daytrip', this.trip.id]);
 		}
   }
 
